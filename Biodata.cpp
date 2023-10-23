6644ef366888565bdff6a214d5c17d2a383b642b
BayuPratama29 <1029bayukun@gmail.com>
2023-10-23 18:14:41 +0700

    menambahkan file Biodata.cpp

---
+++ Biodata.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "----------------------------------------------------------" << endl << endl;
+    cout << "Nama: Muhammad Bayu Pratama" << endl << endl;
+    cout << "NPM: 2310631170032" << endl << endl;
+    cout << "Kelas: 1B IF" << endl << endl;
+    cout << "Cita-Cita: Progammer" << endl << endl;
+    cout << "Motivasi Masuk Informatika: Prospek Kerja yang Fleksibel" << endl << endl;
+    cout << "----------------------------------------------------------" << endl;
+    return 0;
+}
